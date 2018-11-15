9179c2c67f25eaa3769a86c10aeff2b53dbcd2c5
Jon Anders Amundsen <jon-anders.amundsen@payex.com>
2018-11-15 14:37:23 +0100

    Add mostly empty aspnet core mvc project

---
+++ MongoThings/MongoThings.Web/Things/ThingsController.cs
@@ -0,0 +1,22 @@
+﻿#region License
+// --------------------------------------------------
+// Copyright © PayEx. All Rights Reserved.
+// 
+// This software is proprietary information of PayEx.
+// USE IS SUBJECT TO LICENSE TERMS.
+// --------------------------------------------------
+#endregion
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MongoThings.Web.Things
+{
+    public class ThingsController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View("Things/Index.cshtml");
+        }
+    }
+}
